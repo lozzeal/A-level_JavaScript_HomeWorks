95c0a43d5edd87b584f7d96776cf234ca4ecaa18
Alex Honcharov <lozzeal@gmail.com>
2021-01-10 20:52:02 +0200

    added hw4

--- hw1/script.js
+++ hw1/script.js
@@ -1,26 +1,37 @@
-//HW_1.1
-for (let i = 1; i <= 50; i++) {
-   console.log (i);
-};
+// //HW_1.1
+// for (let i = 1; i <= 50; i++) {
+//    console.log (i);
+// };
 
-//HW_1.2
-let i = 1;
-while (i<=50) {
-    console.log(i++)
-};
+// //HW_1.2
+// let i = 1;
+// while (i <= 50) {
+//     console.log(i++);
+// };
 
-//HW_2
-let sum = 0;
-for (var i = 0; i < 5; i++) {
-    sum += i;
- };
-console.log(sum);
+// //HW_2
+// let sum = 0;
+// for (var i = 0; i < 5; i++) {
+//     sum += i;
+//  };
+// console.log(sum);
 
-//HW_3
-let sum = 0;
-for (let i=1 ; i<=20 ; i++) {
-    if(i%2==0) {
-        sum+=i
+// //HW_3
+// let sum = 0;
+// for (let i=1 ; i<=20 ; i++) {
+//     if (i % 2 == 0) {
+//         sum += i;
+//     }
+// };
+// console.log(sum);
+
+//HW_4
+for (let i = 1; i <= 10; i++) {
+    if (i % 3 === 0) {
+        console.log ('FizBuz');
+    } else if (i % 2 === 0) {
+        console.log ('Fiz');
+    } else {
+        console.log('Buz');
     }
 };
-console.log(sum);
