a4c4f082be9708ada99f8d0bcc819c1a1a935d56
Alex Honcharov <lozzeal@gmail.com>
2021-01-10 20:49:56 +0200

    added hw4

--- hw1/script.js
+++ hw1/script.js
@@ -14,7 +14,13 @@ let sum = 0;
 for (var i = 0; i < 5; i++) {
     sum += i;
  };
- console.log(sum);
+console.log(sum);
 
- //HW_3
- 
+//HW_3
+let sum = 0;
+for (let i=1 ; i<=20 ; i++) {
+    if(i%2==0) {
+        sum+=i
+    }
+};
+console.log(sum);
